d9d71fea478f4f29eb55ff88b86dbd3a8210c6ef
jerrellb09 <jerrell.boone.13@gmail.com>
2023-07-24 21:45:22 -0500

    upgraded to angular core 14

--- src/app/components/add-record/add-record.component.ts
+++ src/app/components/add-record/add-record.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
@@ -9,14 +9,14 @@ import { MatDialogRef } from '@angular/material/dialog';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AddRecordComponent implements OnInit {
-  onAdd = new EventEmitter<FormGroup>()
+  onAdd = new EventEmitter<UntypedFormGroup>()
 
   form = this.fb.group({
     name: ['same'],
     cost: [0, [ Validators.min(.01), Validators.required]]
   })
 
-  constructor(private fb: FormBuilder, private dialogRef: MatDialogRef<AddRecordComponent>) { }
+  constructor(private fb: UntypedFormBuilder, private dialogRef: MatDialogRef<AddRecordComponent>) { }
 
   ngOnInit(): void {
 
